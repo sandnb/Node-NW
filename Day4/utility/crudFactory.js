bff2457bebe387fa97c69d2cf1c6eb78bf74ffd4
sandnb <nithinsandilyab@gmail.com>
2024-10-30 21:04:40 +0530

    implemented MVC onto the file

--- Day4/utility/crudFactory.js
+++ Day4/utility/crudFactory.js
@@ -1,11 +1,9 @@
 const  getAllFactory = function (ElementModel){
     return async function (req,res){
         try{
-         console.log("I am inside get method");
-     
         const elementDetails = await ElementModel.find();
          if(elementDetails.length == 0){
-             throw new Error("No Users Found");
+             throw new Error("No Elements Found");
          }
          res.status(200).json({
              status:"success",
@@ -41,9 +39,9 @@ const getByIdFactory = (ElementModel)=>{
     return async function (req,res){
         try{
             const elementId = req.params.elementId;
-        const elementDetails = await ElementModel.findById(elementId);
-        if(elementDetails == "no user found"){
-            throw new Error(`user with ${elementId} not found`)
+            const elementDetails = await ElementModel.findById(elementId);
+        if(elementDetails == "no element found"){
+            throw new Error(`element with ${elementId} not found`)
         }else{
             res.status(200).json({
                 status:"success",
@@ -71,7 +69,7 @@ const deleteByIdFactory = (ElementModel)=>{
         }catch(err){
             res.status(404).json({
                 status:"failure",
-                message:`user with id:${elementId} not found to delete`
+                message:`element with id:${elementId} not found to delete`
             })
         }
     }
